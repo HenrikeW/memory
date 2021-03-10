3e97e6b4d88acc65ca06cb9eb69b24a0e7e4f447
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2021-03-10 16:54:32 +0100

    added functionality to flip cards, check matches, disable and unflip cards, shuffle game

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
 const cards = document.getElementsByClassName("memory-card");
-const cardsArray = Array.from(cards)
+const cardsArray = Array.from(cards);
+
+let oneCardFlipped = false;
+let firstCard, secondCard;
+let boardLocked = false;
 
 const flipCard = (event) => {
-  console.log("clicked")
-  event.currentTarget.classList.toggle("flip")
-}
+  if (boardLocked) return;
+  if (event.currentTarget === firstCard) return;
 
-cardsArray.forEach(card => card.addEventListener("click", flipCard))
+  event.currentTarget.classList.add("flip")
+
+  if (!oneCardFlipped) {
+    oneCardFlipped = true;
+    firstCard = event.currentTarget;
+  } else {
+    oneCardFlipped = false;
+    secondCard = event.currentTarget;
+  }
+
+  checkMatch();
+};
+
+const checkMatch = () => {
+  if (firstCard && secondCard) {
+    boardLocked = true;
+
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+      disableCards();
+    } else {
+      unflipCards();
+    }
+  }
+};
+
+const disableCards = () => {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  setTimeout(() => {
+    firstCard.classList.add("disabled");
+    secondCard.classList.add("disabled");
+    firstCard = null;
+    secondCard = null;
+    boardLocked = false;
+  }, 1500);
+};
+
+const unflipCards = () => {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    firstCard = null;
+    secondCard = null;
+    boardLocked = false;
+  }, 1500)
+};
+
+const shuffle = () => {
+  cardsArray.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 24);
+    card.style.order = randomPosition;
+  })
+}
+shuffle(); 
+
+cardsArray.forEach(card => card.addEventListener("click", flipCard));
